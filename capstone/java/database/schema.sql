12e5ce39c56f785bdcb53e704be5ba9ca79a8eb6
Job Paul <jobpaul18@gmail.com>
2023-01-30 16:45:20 -0500

    SQL DATABASE

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,10 +1,9 @@
 ROLLBACK;
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, band, follower,photo, genre, genre_band, notification, inbox;
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
-	Email varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
