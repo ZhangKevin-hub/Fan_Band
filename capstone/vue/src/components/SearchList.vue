762f78cc45bc62990cbf62dc483d22b7d6c4ce4c
Kevin Zhang <kzhang5832@gmail.com>
2023-02-02 11:17:40 -0500

    update

---
+++ capstone/vue/src/components/SearchList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <input type="text" v-model="searchInput" @input="search"/>
+      <ul>
+        <li v-for="item in filteredBands" :key="item.id">{{ item.name }}</li>
+      </ul>
+    </div>
+  </template>
+<script>
+export default {
+  computed: {
+    filteredBands() {
+      return this.bands.filter(band => {
+        return band.name.toLowerCase().includes(this.searchInput.toLowerCase());
+      });
+    }
+  },
+  data() {
+    return {
+      searchInput: ''
+    }
+  },
+  props: ['bands']
+};
+</script>
+  
