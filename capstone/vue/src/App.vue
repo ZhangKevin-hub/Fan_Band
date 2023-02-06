9f71953188746f3c8d28649a9123a25cf801b567
Joshua Novak <joshuamaxonnovak@gmail.com>
2023-02-06 16:32:58 -0500

    font and background css

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -2,15 +2,32 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'search' }" v-if="$store.state.token != ''">Search</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link
+        v-bind:to="{ name: 'search' }"
+        v-if="$store.state.token != ''"
+        >Search</router-link
+      >&nbsp;|&nbsp;
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
     </div>
     <router-view />
   </div>
 </template>
 
 <style scoped>
-#nav{
+#app {
+  font-family: "Sedgwick Ave Display", "helvetica", cursive;
+   background-image: linear-gradient
+    (to the bottom right, rgb(29, 56, 175), rgb(101, 11, 119));
+}
+#body {
+  background-image: linear-gradient
+    (to the bottom right, rgb(29, 56, 175), rgb(101, 11, 119));
+}
+#nav {
   text-align: right;
   border: 5px solid;
   padding: 10px;
