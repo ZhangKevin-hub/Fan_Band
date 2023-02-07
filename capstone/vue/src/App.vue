a25292a06c54f247da81ed3a649e77615e752cf6
Job Paul <jobpaul18@gmail.com>
2023-02-07 11:39:27 -0500

    background image for home page

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link  v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'search' }" v-if="$store.state.token != ''">Search</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'adminPage' }" v-if="$store.state.token != ''">Admin</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
@@ -10,15 +10,13 @@
   </div>
 </template>
 
-<style scoped>
-#app {
-  font-family: "Sedgwick Ave Display", "helvetica", cursive;
-   background-image: linear-gradient
-    (to the bottom right, rgb(29, 56, 175), rgb(101, 11, 119));
-}
-#body {
-  background-image: linear-gradient
-    (to the bottom right, rgb(29, 56, 175), rgb(101, 11, 119));
+<style>
+
+
+.heading
+{
+    font-family: "Sedgwick Ave Display", "helvetica", cursive;
+
 }
 #nav {
   text-align: right;
--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1 class="heading" >Home</h1>
     <router-link v-bind:to="{ name: 'createBand'}">Create New Band</router-link>
     <home-feed></home-feed>
   </div>
@@ -13,3 +13,15 @@ export default {
   components: { HomeFeed}
 };
 </script>
+<style scoped>
+.home {
+  background-image: url(../assets/images/90sbackground3.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100vh;
+  width: 100vw;
+}
+
+
+</style>
